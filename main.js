792f272f2b19c7c25623c6fc535fe21401c89806
Saurabh <chaudharysaurabh93063@gmail.com>
2022-10-15 12:33:46 +0530

    quick sort

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 let arr=[];
-let flag=false;
-let time=10;
 let fruits;
 let change=document.getElementById("main").offsetWidth/80;
-let j=0;
-let speed=1000;
+let speed=2000/document.getElementById("spd").value;
 visualiser();
 function visualiser(){
 document.getElementById("main").innerHTML="";
@@ -12,8 +9,9 @@ let count=parseInt(document.getElementById("inp").value);
 if (arr.length<count){
 while(arr.length!=count){
     let n=parseInt((Math.random()*200));
+    let flag=false;
 for (let i=0;i<arr.length;i++){
-    flag=false;
+    
     if (n==arr[i] || !n){
         flag=true;
         break;
@@ -37,35 +35,40 @@ for (let i=0;i<arr.length;i++){
     fruits[i].style.height=arr[i]*2+"px";
     fruits[i].style.left=change*i+2+"px";
 }
-j=0;
+chngspeed();
 }
 function chngspeed(){
     speed=2000/document.getElementById("spd").value;
+    for (let i=0;i<arr.length;i++){
+        fruits[i].style.transitionDuration=speed/1000+"s";
+    }
 }
 function start(){
     document.getElementById("start").style.display="none";
     if (document.getElementById("algos").value=="sel"){
         document.getElementById("status").innerHTML="Running Selection Sort";
-        selectionSort();
+        selectionSort(0);
         
     }
     if (document.getElementById("algos").value=="bub"){
         document.getElementById("status").innerHTML="Running Bubble Sort";
         bubbleSort();
     }
+    if (document.getElementById("algos").value=="quick"){
+        document.getElementById("status").innerHTML="Running Quick Sort";
+        quickSort(0,arr.length-1);
+    }
 }
 function swap(x,y){
-    let diff=y-x;
-    document.getElementById(arr[x]).style.transitionDuratio=diff+"s";
     let temp=document.getElementById(arr[x]).style.left;
-    document.getElementById(arr[x]).style.left=document.getElementById(arr[y]).style.left;
-    document.getElementById(arr[y]).style.left=temp;
-    temp=arr[x];
-    arr[x]=arr[y];
-    arr[y]=temp;
+        document.getElementById(arr[x]).style.left=document.getElementById(arr[y]).style.left;
+        document.getElementById(arr[y]).style.left=temp;
+        temp=arr[x];
+        arr[x]=arr[y];
+        arr[y]=temp;
 }
-   
-function selectionSort(){
+
+function selectionSort(j){
         let smallest_i=j;
         for (let i=j+1;i<arr.length;i++){
             if (arr[i]<arr[smallest_i]){
@@ -75,12 +78,12 @@ function selectionSort(){
         swap(smallest_i,j);
         if (j<arr.length-1){
             j++;
-            setTimeout(selectionSort,speed);
+            setTimeout(selectionSort,speed,j);
         }
         
 }
 function bubbleSort(){
-    flag=false;
+    let flag=false;
     for(let i = 0; i < arr.length-1; i++){
         if (arr[i]>arr[i+1]){
             swap(i,i+1);
@@ -89,5 +92,19 @@ function bubbleSort(){
         if (flag){
             setTimeout(bubbleSort,speed);
         }
+}
+function quickSort(low,high){
+    if (low<high){    
+    let k=low-1;
+    let pivot=arr[high];
+    for (let i=low;i<=high;i++){
+        if (arr[i]<=pivot){
+            k++;
+            swap(k,i);
+        }
+    }
+    setTimeout(quickSort,speed,low,k-1);
+    setTimeout(quickSort,speed,k+1,high);
+}
 }
   
