eb75b6d974fb745eb073cd7011274a4b5392a2fb
Saurabh <chaudharysaurabh93063@gmail.com>
2022-10-16 20:21:53 +0530

    insertion sort

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let arr=[];
 let fruits;
 let speed=2000/document.getElementById("spd").value;
+let time=0;
 visualiser();
 function visualiser(){
 
@@ -64,6 +65,10 @@ function start(){
         document.getElementById("status").innerHTML="Running Bubble Sort";
         bubbleSort();
     }
+    if (document.getElementById("algos").value=="ins"){
+        document.getElementById("status").innerHTML="Running Insertion Sort";
+        insertionSort(0);
+    }
     if (document.getElementById("algos").value=="quick"){
         document.getElementById("status").innerHTML="Running Quick Sort";
         quickSort(0,arr.length-1);
@@ -75,13 +80,16 @@ function reset(){
     document.getElementById("start").style.display="inline";
     visualiser();
 }
-function done(){
+function done(func){
+    clearInterval(func);
     document.getElementById("status").innerHTML="done";
     document.getElementById("inp").style.display="inline";
     document.getElementById("reset").style.display="inline";
     document.getElementById("algos").style.display="inline";
+    time=0;
 }
 function swap(x,y){
+    chngspeed();
     let temp=document.getElementById(arr[x]).style.left;
         document.getElementById(arr[x]).style.left=document.getElementById(arr[y]).style.left;
         document.getElementById(arr[y]).style.left=temp;
@@ -108,18 +116,39 @@ function selectionSort(j){
         
 }
 function bubbleSort(){
+    let func=setInterval(function(){
     let flag=false;
     for(let i = 0; i < arr.length-1; i++){
         if (arr[i]>arr[i+1]){
-            swap(i,i+1);
+            let temp=document.getElementById(arr[i]).style.left;
+        document.getElementById(arr[i]).style.left=document.getElementById(arr[i+1]).style.left;
+        document.getElementById(arr[i+1]).style.left=temp;
+        temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
             flag=true;      
         }}
-        if (flag){
-            setTimeout(bubbleSort,speed);
+        if (!flag){
+            done(func);
         }
-        else{
-            done();
+    },speed);
+}
+function insertionSort(j){
+    let func=setInterval(function(){
+    let flag=false;
+    for(let i = 0; i < j; i++){
+        if (arr[i]>arr[i+1]){
+            swap(i,i+1);
+            flag=true; 
         }
+        }
+        if (!flag){        
+        j++;
+        }
+        if (j>=arr.length){
+            done(func);
+        } 
+    },speed);
 }
 function quickSort(low,high){
     if (low<high){    
